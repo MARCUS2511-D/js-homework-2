8da27571cdc3cac4788b171cdb05ead6deefb5ed
MARCUS2511-D <92095745+MARCUS2511-D@users.noreply.github.com>
2021-11-11 15:18:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+var age = +prompt("Yoshingizni kiriting")
+
+if (age <= 10){
+    alert("Xali go'dak ekansiz")
+} else if (age <= 25){
+    alert("Xali yosh ekansiz o'qishingiz kerak");
+} else if (age <= 45){
+    alert("Soqqani bosish kerak bu yoshda");
+} else if (age <=65){
+    alert("Pensiyaga chiqadigon vaqtiz kelib qopti sekin damni olorin");
+} else if (age <=100) 
+{
+    alert("Haliyam yuribsimi pensiyaga chiqin endi zeriktrvordzu bo'lmasa u kunlarni ko'rme qolasiz");
+}else{
+    alert("Nimadur xato keti, qaytadan urinib ko'ring")
+}
+
+
+
